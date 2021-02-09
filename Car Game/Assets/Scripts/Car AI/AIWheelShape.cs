f0269e996149acc0577b31a1db3048978ba58a14
gaziakvrn <gaziakvrn@gmail.com>
2021-02-09 10:23:19 +0300

    Backup
    The last backup was taken before attempting to upgrade to "2020.2.3f1" for corrective action due to a bug in Unity version "2019.4.14.f1".

---
+++ Car Game/Assets/Scripts/Car AI/AIWheelShape.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIWheelShape : MonoBehaviour
+{
+	[Tooltip("If you need the visual wheels to be attached automatically, drag the wheel shape here.")]
+	public GameObject wheelShape;
+
+	private WheelCollider[] m_Wheels;
+
+	// Find all the WheelColliders down in the hierarchy.
+	void Start()
+	{
+		m_Wheels = GetComponentsInChildren<WheelCollider>();
+
+		for (int i = 0; i < m_Wheels.Length; ++i)
+		{
+			var wheel = m_Wheels[i];
+
+			// Create wheel shapes only when needed.
+			if (wheelShape != null)
+			{
+				var ws = Instantiate(wheelShape);
+				ws.transform.parent = wheel.transform;
+			}
+		}
+	}
+
+    private void Update()
+    {
+		foreach (WheelCollider wheel in m_Wheels)
+		{
+			
+
+			// Update visual wheels if any.
+			if (wheelShape)
+			{
+				Quaternion q;
+				Vector3 p;
+				wheel.GetWorldPose(out p, out q);
+
+				// Assume that the only child of the wheelcollider is the wheel shape.
+				Transform shapeTransform = wheel.transform.GetChild(0);
+
+				if (wheel.name == "a0l" || wheel.name == "a1l" || wheel.name == "a2l")
+				{
+					shapeTransform.rotation = q * Quaternion.Euler(0, 180, 0);
+					shapeTransform.position = p;
+				}
+				else
+				{
+					shapeTransform.position = p;
+					shapeTransform.rotation = q;
+				}
+			}
+		}
+	}
+}
---
+++ Car Game/Assets/Scripts/Car AI/CarSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject[] carPrefabs;
+
+    void Start()
+    {
+        Instantiate(SelectACarPrefab(), transform);
+    }
+
+    private GameObject SelectACarPrefab()
+    {
+        var randomIndex = Random.Range(0, carPrefabs.Length);
+        return carPrefabs[randomIndex];
+    }
+}
